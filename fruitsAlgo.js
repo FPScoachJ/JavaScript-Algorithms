e4516d111c7d90806ab52eab5d754f1fce7e79f6
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-04-28 09:08:21 -0700

    fruits-algorithm
    Completed code

---
+++ fruitsAlgo.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of strings as an argument and returns a new array with all the strings sorted in alphabetical order.
+
+let fruits = ["apple", "banana", "orange", "grape", "kiwi"];
+let newFruits = [];
+
+function stringSort(fruits){
+    newFruits = fruits.sort();
+    return newFruits;
+    }
