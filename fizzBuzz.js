492b7d7fbfd4bfa3aa4c58293d477d1068ebbbe3
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-04-28 08:25:04 -0700

    fizzBuzz Switch
    Added switch statement as alternative.

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,10 +1,30 @@
+// function fizzBuzz(i){
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("fizz buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("buzz");
+//   } else console.log(i);
+// }
 
-for (let i = 1; i <= 99; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log("fizz buzz");
-  } else if (i % 3 == 0) {
-    console.log("fizz");
-  } else if (i % 5 == 0) {
-    console.log("buzz");
-  } else console.log(i);
+// fizzBuzz(15);
+
+function fizzBuzz(i){
+  switch (true) {
+    case (i % 3 === 0 && i % 5 === 0):
+      console.log("fizz buzz");
+      break;
+    case (i % 3 === 0):
+      console.log("fizz");
+      break;
+    case (i % 5 === 0):
+      console.log("buzz");
+      break;
+    default:
+      console.log(i);
+      break;
+  }
 }
+
+fizzBuzz(99);
