9259b836a9fee7158c328fe23a62cd5f97792284
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-05-02 09:25:54 -0700

    Added Joe's Solutions

--- palindromeAlgo.js
+++ palindromeAlgo.js
@@ -1,12 +1,12 @@
 //Write a function that takes a string as input and returns true if the string is a palindrome (a word, phrase, or sequence that reads the same backwards as forwards).
 
-let str1 = "abba";
-let str2 = "tacocat";
-let str3 = "abbc";
+const str1 = "abba";
+
 
 function reverseStr(str1) {
-  let reverseStr1 = "";
-  reverseStr2 = str1.reverse();
+  const splitStr1 = str1.split("");
+  const reverseStr1 = splitStr1.reverse();
+  reverseStr1 = str1.join("");
   if (reverseStr1 === str1) {
     return true;
   } else {
@@ -15,3 +15,31 @@ function reverseStr(str1) {
 }
 
 reverseStr(str1);
+
+// using built in methods
+function palindromeChecker(str) {
+  const reversedWord = str.split("").reverse().join("");
+  if (reversedWord === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// reverse loop
+function palindromeChecker2(str) {
+  let reversedString = "";
+  for (let index = str.length - 1; index >= 0; index--) {
+    reversedString += str[index];
+  }
+  if (reversedString === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// one liner
+const isPalindrome = (str) => str.split("").reverse().join("");
+
+console.log(palindromeChecker2("racecar"));
+console.log(palindromeChecker2("book"));
+console.log(palindromeChecker2("tacocat"));
--- stringToObjectAlgo.js
+++ stringToObjectAlgo.js
@@ -12,3 +12,16 @@ function letterCount(string){
     return stringObject;
 }
 console.log(stringObject[string[i]]) 
+
+function findLetters2(string) {
+  const hashmap = {};
+  for (let index = 0; index < string.length; index++) {
+    const letter = string[index];
+    if (!hashmap[letter]) {
+      hashmap[letter] = 1;
+    } else {
+      hashmap[letter]++;
+    }
+  }
+  return hashmap;
+}
